192512ac378ebcde4984a0614bf8b49b090c0cd3
Nika <freexnick@yahoo.com>
2022-09-06 17:40:09 +0400

    feat:jwt_setup

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,7 +3,10 @@ const { StatusCodes } = require("http-status-codes");
 
 const register = async (req, res) => {
   const user = await User.create({ ...req.body });
-  res.status(StatusCodes.CREATED).json({ user });
+  const token = user.createJWT();
+  res
+    .status(StatusCodes.CREATED)
+    .json({ user: { name: user.getName() }, token });
 };
 const login = async (req, res) => {
   res.send("login user");
