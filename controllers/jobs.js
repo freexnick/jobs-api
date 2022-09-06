e53859a6152d3a5a6fd836f614fbb4e06600fd4b
Nika <freexnick@yahoo.com>
2022-09-06 20:24:18 +0400

    feat:create_job

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -1,15 +1,25 @@
+const Job = require("../models/Job");
+const { StatusCodes } = require("http-status-codes");
+const { BadRequestError, NotFoundError } = require("../errors");
+
 const getAllJobs = async (req, res) => {
   res.send("get all jobs");
 };
+
 const getJob = async (req, res) => {
   res.send("get job");
 };
+
 const createJob = async (req, res) => {
-  res.send("create job");
+  req.body.createdBy = req.user.userId;
+  const job = await Job.create(req.body);
+  res.status(StatusCodes.CREATED).json({ job });
 };
+
 const updateJob = async (req, res) => {
   res.send("get update job");
 };
+
 const deleteJob = async (req, res) => {
   res.send("delete job");
 };
