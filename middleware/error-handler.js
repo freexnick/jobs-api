24d0167750e0918724eefb5e7b716e9c3efc52ab
Nika <freexnick@yahoo.com>
2022-09-07 17:31:37 +0400

    feat:duplicate_err

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,10 +1,16 @@
-const { CustomAPIError } = require('../errors')
-const { StatusCodes } = require('http-status-codes')
+const { StatusCodes } = require("http-status-codes");
 const errorHandlerMiddleware = (err, req, res, next) => {
-  if (err instanceof CustomAPIError) {
-    return res.status(err.statusCode).json({ msg: err.message })
-  }
-  return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err })
-}
+  let customError = {
+    statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+    msg: err.message || "Something went wrong, try again later",
+  };
 
-module.exports = errorHandlerMiddleware
+  if (err.code && err.code === 11000) {
+    customError.msg = `${Object.keys(err.keyValue)} already exist`;
+    customError.statusCode = 400;
+  }
+
+  return res.status(customError.statusCode).json({ msg: customError.msg });
+};
+
+module.exports = errorHandlerMiddleware;
