294a68abcad78e74c2675c8e8fca691d1eba5127
Nika <freexnick@yahoo.com>
2022-09-07 23:09:31 +0400

    feat:security_setup

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 require("dotenv").config();
 require("express-async-errors");
+
+const helmet = require("helmet");
+const cors = require("cors");
+const xss = require("xss-clean");
+const rateLimit = require("express-rate-limit");
+
 const express = require("express");
 const app = express();
 
@@ -13,6 +19,15 @@ const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
 
 app.use(express.json());
+app.use(helmet());
+app.use(cors());
+app.use(xss());
+app.use(
+  rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 100,
+  })
+);
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/jobs", authenticateUser, jobsRouter);
